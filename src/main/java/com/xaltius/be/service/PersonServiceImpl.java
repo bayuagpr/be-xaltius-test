fba490cc21bd1207523e325542adba908a1c7a5d
Bayu Prakoso <bayu.prakoso.workid@gmail.com>
2020-12-09 18:32:02 +0700

    Improve incorrect age exception handling

--- src/main/java/com/xaltius/be/service/PersonServiceImpl.java
+++ src/main/java/com/xaltius/be/service/PersonServiceImpl.java
@@ -34,7 +34,7 @@ public class PersonServiceImpl implements PersonService{
                 return Colors.GREY.getHexCode();
             }
         }
-        throw new IncorrectAgeException(age+" years old doesn't meet requirement for generating. Minimum age is 1 years old.");
+        throw new IncorrectAgeException("Sorry, "+age+" years old doesn't meet requirement for generating. Minimum age is 1 years old.");
     }
 
     private String checkNameValidity(String name) throws IncorrectNameException {
